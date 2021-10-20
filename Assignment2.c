6ee5eba1a088ae13ff358dd0d6abbfa446c96c58
luke <hoskam@pdx.edu>
2021-10-20 09:17:53 -0700

    Problem 1 works... But could be better.

--- Assignment2.c
+++ Assignment2.c
@@ -1,6 +1,91 @@
-#include <stdio.h>
+//luke hoskam
+//ECE361
+//Assignment 2
+//Dr. Kravits
+//
+//
+//This program will allow a user to input a 5x5 array of integers
+//and tabulate the rows and the columns
+//
 
-int main()
+
+#include <stdio.h>
+#include <stdlib.h>
+#define NUM_COLS 5
+#define NUM_ROWS 5
+
+int rowTally(int matrix[NUM_ROWS][NUM_COLS], int rowNum);
+int colTally(int matrix[NUM_ROWS][NUM_COLS], int ColNum);
+
+
+int main(void)
 {
-    printf("Hello World!");
+    int ch;
+    int myMatrix[NUM_ROWS][NUM_COLS];
+    int rowTotals[NUM_ROWS];
+    int colTotals[NUM_COLS];
+
+    printf("The program will only accept the first five values \n");
+    for(int i = 0; i < NUM_ROWS;i++)
+    {
+        printf("Enter 5 values for row %d:", (i+1));
+        for(int j = 0; j < NUM_COLS; j++)
+        {
+            scanf("%d", &myMatrix[i][j]);
+            
+        }
+        while ((ch = getchar()) != '\n');
+    }
+    printf("the input matrix is: \n");
+    for(int i = 0; i < NUM_ROWS;i++)
+    {
+        //printf("Values for row %d: ", (i+1));
+        for(int j = 0; j < NUM_COLS; j++)
+        {
+            printf("%d  ", myMatrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    for(int i =0; i < NUM_ROWS; i++)
+    {
+        rowTotals[i] = rowTally(myMatrix,i);
+    }
+
+    for(int i = 0; i< NUM_ROWS; i++)
+    {
+        printf("Row %d total: %d \n", (i+1),rowTotals[i]);
+    }
+
+    for(int i =0; i < NUM_COLS; i++)
+    {
+        colTotals[i] = colTally(myMatrix,i);
+    }
+
+    for(int i = 0; i< NUM_COLS; i++)
+    {
+        printf("column %d total: %d \n", (i+1),colTotals[i]);
+    }
+
+    return 0;
+}
+
+int rowTally(int matrix[NUM_ROWS][NUM_COLS], int rowNum)
+{
+    int total = 0;
+    for(int i = 0; i < NUM_COLS; i ++)
+    {
+        total += matrix[rowNum][i];
+    }
+    return total;
+}
+
+int colTally(int matrix[NUM_ROWS][NUM_COLS], int colNum)
+{
+    int total = 0;
+    for(int i = 0; i < NUM_ROWS; i ++)
+    {
+        total += matrix[i][colNum];
+    }
+    return total;
 }
